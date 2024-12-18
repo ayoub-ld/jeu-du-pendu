b54f41a60ec14f2ba503cc86797f00111aa8863a
Ayoub <lamkadam.ab@gmail.com>
2024-12-18 09:46:36 +0100

    Ajout de plusieurs fonctions, le jeu s'arrête au bon moment,  manque affichage des lettres et comportement quand toutes les lettres sont trouvées

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ const btn = document.querySelector("button")!;
 const container = document.querySelector(".container") as HTMLDivElement;
 const lettre = document.querySelector("input") as HTMLInputElement;
 const wordContainer = document.querySelector(".word") as HTMLDivElement;
+const penduImg = document.querySelector(".pendu-img") as HTMLImageElement;
+const nbLettresContainer = document.querySelector(
+  "#nbLettres"
+) as HTMLDivElement;
+let lettresDejaTrouves: string[] = [];
 
 // Variables
 const listeDeMots: string[] = [
@@ -71,9 +76,15 @@ let compteur: number = 0;
  */
 function play(mot: string, lettre: string): void {
   if (compteur < 6) {
+    // displayNombreLettres(motATrouver);
     if (mot.includes(lettre)) {
-      console.log("La lettre est dans le mot");
-      bonneLettre(lettre);
+      if (lettresDejaTrouves.includes(lettre)) {
+        console.log("Lettre déjà trouvée !");
+        console.log("liste des lettres déjà trouvées: " + lettresDejaTrouves);
+      } else {
+        console.log("La lettre est dans le mot");
+        bonneLettre(lettre);
+      }
     } else {
       console.log("La lettre n'est pas dans le mot");
       mauvaiseLettre(lettre);
@@ -89,6 +100,7 @@ function play(mot: string, lettre: string): void {
 function mauvaiseLettre(lettre: string) {
   compteur++;
   console.log("Mauvaise lettre - Plus que " + (6 - compteur) + " tentatives");
+  penduImg.src = `/images/pendu-${compteur}.png`;
 }
 
 /**
@@ -96,7 +108,15 @@ function mauvaiseLettre(lettre: string) {
  * @param lettre - La lettre qui n'est pas présente dans le motATrouver
  * @description Fonction pour les bonnes lettres entrées par l'utilisateur
  */
-function bonneLettre(lettre: string) {}
+function bonneLettre(lettre: string) {
+  console.log(
+    "Bien joué, la lettre " +
+      lettre +
+      " vient d'être ajoutée aux lettres trouvées"
+  );
+  lettresDejaTrouves.push(lettre);
+  console.log("Lettres trouvées: " + lettresDejaTrouves);
+}
 
 /**
  * @name partiePerdue
@@ -111,10 +131,39 @@ function partiePerdue() {
   wordContainer.appendChild(p);
 }
 
-// Event listeners
-btn.addEventListener("click", (event) => {
-  const randomNumber = Math.floor(Math.random() * listeDeMots.length);
-  motATrouver = listeDeMots[randomNumber];
+/**
+ * @name displayLettresDejaTrouves
+ * @description Fonction pour afficher les lettres déjà trouvées
+ */
+function displayLettresDejaTrouves(): void {
+  for (let lettres of lettresDejaTrouves) {
+    const p = document.createElement("p");
+    p.classList.add("list-lettres-trouvees");
+    p.textContent = lettres;
+    nbLettresContainer.append(p);
+  }
+}
 
+/**
+ * @name displayNombreLettres
+ * @param mot - Mot à trouver
+ * @description Fonction pour afficher le nombre de lettres du mot à trouver
+ */
+function displayNombreLettres(mot: string): void {
+  for (let i = 0; i < mot.length; i++) {
+    const p = document.createElement("p");
+    p.classList.add("lettre-cachee");
+    p.textContent = "_";
+    nbLettresContainer.appendChild(p);
+  }
+}
+
+// Event listeners
+const randomNumber = Math.floor(Math.random() * listeDeMots.length);
+motATrouver = listeDeMots[randomNumber];
+console.log("TRICHE: LE MOT EST " + motATrouver);
+
+displayLettresDejaTrouves();
+btn.addEventListener("click", (event) => {
   play(motATrouver, lettre.value);
 });
